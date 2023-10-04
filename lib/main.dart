e6fbbaba26e83745632486f712673d129590df1a
Thania Vivek <tvivek1@sheffield.ac.uk>
2023-10-04 23:39:09 +0100

    save login state

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tb_deliveryapp/services/auth_manager.dart';
 import 'package:tb_deliveryapp/views/loginAuth/login_page.dart';
+import 'package:tb_deliveryapp/views/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,11 +11,40 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
+  final AuthManager authManager = AuthManager(); // Create an instance of AuthManager
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Tummy Box Partner App',
-      home: LoginPage(),
+      theme: ThemeData(
+        // Define your app's theme here
+        // For example, you can set primary colors, fonts, etc.
+      ),
+      home: FutureBuilder<bool>(
+        future: authManager.isUserLoggedIn(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator(); // You can show a loading indicator here
+          } else {
+            final bool isLoggedIn = snapshot.data ?? false;
+
+            return FutureBuilder<String?>(
+              future: authManager.getPartnerId(),
+              builder: (context, partnerIdSnapshot) {
+                final String? partnerId = partnerIdSnapshot.data ?? "";
+
+                // Check if the user is authenticated and decide which screen to display
+                final Widget initialRoute = isLoggedIn
+                    ? HomeView(isLoggedIn: false, partnerId: partnerId ?? "")
+                    : LoginPage();
+
+                return initialRoute;
+              },
+            );
+          }
+        },
+      ),
     );
   }
 }
