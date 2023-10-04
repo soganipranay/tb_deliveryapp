04030d5b0058cf96942288f30dd13651ea5aa883
i-akshat-jain <108737870+akshatdebtenforce@users.noreply.github.com>
2023-10-04 18:58:02 +0100

    User Details Done

--- lib/services/firebase_service.dart
+++ lib/services/firebase_service.dart
@@ -9,50 +9,50 @@ class FirebaseService {
   final CollectionReference userPartners =
       FirebaseFirestore.instance.collection('Users');
   final CollectionReference<Map<String, dynamic>> officeLocation =
-      FirebaseFirestore.instance.collection('Office');
+      FirebaseFirestore.instance.collection('Offices');
 
   final CollectionReference<Map<String, dynamic>> schoolLocation =
-      FirebaseFirestore.instance.collection('School');
+      FirebaseFirestore.instance.collection('Schools');
 
-  Future<int> fetchTotalOrders(String locations, String orderType) async {
-    try {
-      final CollectionReference ordersCollection =
-          FirebaseFirestore.instance.collection('Orders');
-      DateTime currentDate = DateTime.now();
-      int year = currentDate.year; // Year component
-      int month = currentDate.month; // Month component (1 to 12)
-      int day = currentDate.day; // Day component
-      late int totalOrders = 0;
+  // Future<int> fetchTotalOrders(String locations, String orderType) async {
+  //   try {
+  //     final CollectionReference ordersCollection =
+  //         FirebaseFirestore.instance.collection('Orders');
+  //     DateTime currentDate = DateTime.now();
+  //     int year = currentDate.year; // Year component
+  //     int month = currentDate.month; // Month component (1 to 12)
+  //     int day = currentDate.day; // Day component
+  //     late int totalOrders = 0;
 
-      currentDate = DateTime(year, month, day);
+  //     currentDate = DateTime(year, month, day);
 
-      DateTime nextDate = currentDate.add(const Duration(days: 1));
+  //     DateTime nextDate = currentDate.add(const Duration(days: 1));
 
-      print('Current Date: $currentDate');
-      print('Next Date: $nextDate');
+  //     print('Current Date: $currentDate');
+  //     print('Next Date: $nextDate');
 
-      QuerySnapshot querySnapshot = await ordersCollection
-          .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
-          .where('deliveryDate', isLessThan: nextDate)
-          .where('orderType', isEqualTo: orderType)
-          .where('location', isEqualTo: locations)
-          .get();
+  //     QuerySnapshot querySnapshot = await ordersCollection
+  //         .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
+  //         .where('deliveryDate', isLessThan: nextDate)
+  //         .where('orderType', isEqualTo: orderType)
+  //         .where('location', isEqualTo: locations)
+  //         .get();
 
-      querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
-        // Get the reference of each document
-        DocumentReference documentReference = documentSnapshot.reference;
-        print('Document Path: ${documentReference.path}');
-        // get the count of the documents
-        totalOrders = querySnapshot.docs.length;
-        print('Total Orders: $totalOrders');
-      });
+  //     querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
+  //       // Get the reference of each document
+  //       DocumentReference documentReference = documentSnapshot.reference;
+  //       print('Document Path: ${documentReference.path}');
+  //       // get the count of the documents
+  //       totalOrders = querySnapshot.docs.length;
+  //       print('Total Orders: $totalOrders');
+  //     });
 
-      return totalOrders;
-    } catch (e) {
-      print("Error fetching total order references: $e");
-      return 0;
-    }
-  }
+  //     return totalOrders;
+  //   } catch (e) {
+  //     print("Error fetching total order references: $e");
+  //     return 0;
+  //   }
+  // }
 
   Future<Map<String, dynamic>> fetchOrderByOrderStatus(
       String orderStatus, String location, String orderType) async {
@@ -138,30 +138,6 @@ class FirebaseService {
       print('Error updating orders in Firestore: $e');
       // Handle the error as needed
     }
-  }
-
-  Future<Map<String, dynamic>> fetchTimeForScanning() async {
-    final timeDocs = await timeCollection.get();
-
-    final Map<String, dynamic> timeSlots = {};
-    String timestampToFormattedTime(Timestamp timestamp) {
-      DateTime dateTime = timestamp.toDate();
-      String formattedTime =
-          "${dateTime.hour.toString().padLeft(2, '0')}:${dateTime.minute.toString().padLeft(2, '0')}:${dateTime.second.toString().padLeft(2, '0')}";
-      return formattedTime;
-    }
-
-    timeDocs.docs.forEach((doc) {
-      final data = doc.data() as Map<String, dynamic>;
-      final slotName = doc.id;
-      timeSlots[slotName] = {
-        'startTime': timestampToFormattedTime(data['StartTime']),
-        'endTime': timestampToFormattedTime(data['EndTime']),
-      };
-    });
-
-    print('Time Slots: $timeSlots');
-    return timeSlots;
   }
 
   Future<String?> getDeliveryPartnerId(
@@ -254,11 +230,37 @@ class FirebaseService {
     }
   }
 
+Future<Map<String, dynamic>> getPartnerDetails(String partnerId) async {
+    try {
+      DocumentReference docRef = userPartners.doc(partnerId);
+      DocumentSnapshot docSnapshot = await docRef.get();
+
+      if (docSnapshot.exists) {
+        final data = docSnapshot.data() as Map<String, dynamic>;
+        return {
+          'name': docSnapshot.reference.id,
+          'display_name': data['display_name'],
+          'email': data['email'],
+          'locations': data['locations'],
+          'phone_number': data['phone_number'],
+          'photo_url': data['photo_url'],
+          'userType': data['userType'],
+        };
+      } else {
+        // Handle the case where the document doesn't exist.
+        return {};
+      }
+    } catch (e) {
+      print('Error: $e');
+      return {}; // Handle the error gracefully.
+    }
+  }
+
   Future<List<dynamic>?> getLocationsForPartnerId(String partnerId) async {
     try {
       DocumentReference docRef = userPartners.doc(partnerId);
       DocumentSnapshot docSnapshot = await docRef.get();
-      print("partnerId $docSnapshot");
+      print("partnerId ${docSnapshot.data}");
       List<String> locationsForDeliveryPartner =
           List<String>.from(docSnapshot['locations'] ?? []);
 
@@ -272,40 +274,32 @@ class FirebaseService {
   Future<List<String>?> fetchLocationNamesByLocationIds(
       List<dynamic>? locations) async {
     List<String> locationNames = [];
-    List<String> extracteResults = [];
-    for (String locationRef in locations ?? []) {
-      List<String> parts = locationRef.split('/');
-      if (parts.length > 2) {
-        String result = parts.sublist(2).join('/');
-        extracteResults.add(result);
-        print("result $result"); // This will print the extracted substring
-      } else {
-        return null;
-      }
-    }
-    print(
-        "extracteResults $extracteResults"); // This will print the extracted substring
-    // extracteResults [R3whnAMDi374RJBvE67t, nYTz12ahki2R0rAwZzqz, MJrR2geqczDf3kRTiOxt]
+
     try {
-      for ( String? locationRef in extracteResults) {
-        // Check if the location is in the officeLocation collection
-        DocumentSnapshot<Map<String, dynamic>> officeDoc =
-            await officeLocation.doc(locationRef).get();
-        print("locationRef $locationRef");
-        if (officeDoc.exists) {
-          locationNames.add("${officeDoc.data()!['name']}");
-        } else {
-          // Check if the location is in the schoolLocation collection
-          DocumentSnapshot<Map<String, dynamic>> schoolDoc =
-              await schoolLocation.doc(locationRef).get();
-          if (schoolDoc.exists) {
-            locationNames.add("${schoolDoc.data()!['name']}");
+      if (locations != null) {
+        for (String locationRef in locations) {
+          // Check if the location is in the officeLocation collection
+          DocumentReference officeDoc = await officeLocation.doc(locationRef);
+          DocumentSnapshot officeSnapshot = await officeDoc.get();
+          print("locationRef $locationRef");
+          if (officeSnapshot.exists) {
+            final dynamic officeData = officeSnapshot.data(); // Cast to dynamic
+            locationNames.add("${officeData["name"]}");
           } else {
-            // Print a message when neither officeDoc nor schoolDoc exists
-            print("Neither officeDoc nor schoolDoc exists for $locationRef");
+            // Check if the location is in the schoolLocation collection
+            DocumentReference schoolDoc = await schoolLocation.doc(locationRef);
+            DocumentSnapshot schoolSnapshot = await schoolDoc.get();
+            if (schoolSnapshot.exists) {
+              final dynamic schoolData =
+                  schoolSnapshot.data(); // Cast to dynamic
+              locationNames.add("${schoolData["name"]}");
+            } else {
+              // Print a message when neither officeDoc nor schoolDoc exists
+              print("Neither officeDoc nor schoolDoc exists for $locationRef");
+            }
           }
+          print("locationNames: $locationNames");
         }
-        print("locationNames: $locationNames");
       }
     } catch (e) {
       print('Error: $e');
@@ -314,77 +308,3 @@ class FirebaseService {
     return locationNames;
   }
 }
-
-      // if (DateTime.parse(currentTime)
-      //         .isAfter(DateTime.parse(timeSlots['breakfast']['startTime'])) &&
-      //     DateTime.parse(currentTime)
-      //         .isBefore(DateTime.parse(timeSlots['breakfast']['endTime']))) {
-      //   print('Current Time: ${DateTime.parse(currentTime)}');
-      //   if (profileType == "Child") {
-      //     QuerySnapshot querySnapshot = await ordersCollection
-      //         .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
-      //         .where('deliveryDate', isLessThan: nextDate)
-      //         .where('orderType', whereIn: ['breakfast', 'lunch'])
-      //         .where('location')
-      //         .where('profileType', isEqualTo: profileType)
-      //         .get();
-
-      //     querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
-      //       // Get the reference of each document
-      //       DocumentReference documentReference = documentSnapshot.reference;
-      //       print('Document Path: ${documentReference.path}');
-      //       // get the count of the documents
-      //       totalOrders = querySnapshot.docs.length;
-      //     });
-      //   } else if (profileType == "Adult") {
-      //     QuerySnapshot querySnapshot = await ordersCollection
-      //         .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
-      //         .where('deliveryDate', isLessThan: nextDate)
-      //         .where('orderType', isEqualTo: 'breakfast')
-      //         .where('profileType', isEqualTo: profileType)
-      //         .get();
-      //     querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
-      //       // Get the reference of each document
-      //       DocumentReference documentReference = documentSnapshot.reference;
-      //       print('Document Path: ${documentReference.path}');
-      //       // get the count of the documents
-      //       totalOrders = querySnapshot.docs.length;
-      //     });
-      //   }
-      // } else if (DateTime.parse(currentTime)
-      //         .isAfter(DateTime.parse(timeSlots['lunch']['startTime'])) &&
-      //     DateTime.parse(currentTime)
-      //         .isBefore(DateTime.parse(timeSlots['lunch']['endTime']))) {
-      //   QuerySnapshot querySnapshot = await ordersCollection
-      //       .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
-      //       .where('deliveryDate', isLessThan: nextDate)
-      //       .where('orderType', isEqualTo: 'lunch')
-      //       .where('profileType', isEqualTo: 'Adult')
-      //       .get();
-
-      //   querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
-      //     // Get the reference of each document
-      //     DocumentReference documentReference = documentSnapshot.reference;
-      //     print('Document Path: ${documentReference.path}');
-      //     // get the count of the documents
-      //     totalOrders = querySnapshot.docs.length;
-      //   });
-      // } else if (DateTime.parse(currentTime)
-      //         .isAfter(DateTime.parse(timeSlots['dinner']['startTime'])) &&
-      //     DateTime.parse(currentTime)
-      //         .isBefore(DateTime.parse(timeSlots['dinner']['endTime']))) {
-      //   QuerySnapshot querySnapshot = await ordersCollection
-      //       .where('deliveryDate', isGreaterThanOrEqualTo: currentDate)
-      //       .where('deliveryDate', isLessThan: nextDate)
-      //       .where('orderType', isEqualTo: 'dinner')
-      //       .where('profileType', isEqualTo: 'Adult')
-      //       .get();
-
-      //   querySnapshot.docs.forEach((QueryDocumentSnapshot documentSnapshot) {
-      //     // Get the reference of each document
-      //     DocumentReference documentReference = documentSnapshot.reference;
-      //     print('Document Path: ${documentReference.path}');
-      //     // get the count of the documents
-      //     totalOrders = querySnapshot.docs.length;
-      //   });
-      // }
--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -14,7 +14,7 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   List<dynamic>? deliveryPartnerLocations = []; // Change the data type here
-
+  Map<String, dynamic> partnerDetails = {};
   @override
   void initState() {
     super.initState();
@@ -23,12 +23,16 @@ class _HomeViewState extends State<HomeView> {
 
   Future<void> initializeData() async {
     FirebaseService firebaseService = FirebaseService();
-    List<dynamic>? locations = await firebaseService
-        .getLocationsForPartnerId(widget.partnerId);
+    List<dynamic>? locations =
+        await firebaseService.getLocationsForPartnerId(widget.partnerId);
+    Map<String, dynamic> details =
+        await firebaseService.getPartnerDetails(widget.partnerId);
 
     if (locations != null) {
       setState(() {
         deliveryPartnerLocations = locations;
+        partnerDetails = details; // Store the details in the state
+        print("Partner Details: $partnerDetails");
         print("Home deliveryPartnerLocations $deliveryPartnerLocations");
       });
     }
@@ -51,6 +55,12 @@ class _HomeViewState extends State<HomeView> {
       body: Container(
         child: Column(
           children: [
+
+                    Text('Name: ${partnerDetails['display_name']}'),
+                    Text('Email: ${partnerDetails['email']}'),
+                    Text('Phone Number: ${partnerDetails['phone_number']}'),
+                    // Add more details as needed
+                  
             ElevatedButton(
               onPressed: () {
                 Navigator.push(
